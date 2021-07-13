11168d514af914bd8c26f7aaf99efca7bb32b0b4
ldthomp <70942973+ldthomp@users.noreply.github.com>
2021-07-13 14:52:55 +0100

    Initial Commit + Main Concept

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    Rigidbody rigidbody;
+    Vector3 playerPos;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        playerPos = transform.position;    
+    }
+
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            print(Input.GetAxis("Horizontal") + Time.deltaTime);
+            //rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            rigidbody.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            rigidbody.AddRelativeForce(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            rigidbody.AddRelativeForce(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+            rigidbody.AddRelativeForce(Vector3.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            ResetRBConstraints();
+        }
+        
+    }
+
+    private void ResetRBConstraints()
+    {
+        rigidbody.constraints = RigidbodyConstraints.None;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+}
---
+++ Assets/Scripts/Core/PathManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] GameObject blockExplosionVFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("Entering Block");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(rb != null)
+            {
+                Instantiate(blockExplosionVFX, transform.position, transform.rotation);
+                rb.useGravity = true;
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+}
