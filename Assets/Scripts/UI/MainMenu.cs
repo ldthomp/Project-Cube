02387868fd90ae1be69073aa71c9a5d0176270f8
ldthomp <70942973+ldthomp@users.noreply.github.com>
2021-07-18 20:50:52 +0100

    Main Menu Part finished

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    Scene currentScene;
+    public Canvas controlsOverlay;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && controlsOverlay.enabled == true)
+        {
+            controlsOverlay.enabled = false;
+        }
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Controls()
+    {
+       
+        controlsOverlay.enabled = true;
+       
+    }
+}
